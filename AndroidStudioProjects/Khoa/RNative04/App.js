3fad0d0ce5d4bc640b9b42465989d0e3a8a98c0b
KTheAsianimeBoi <86539636+KTheAsianimeBoi@users.noreply.github.com>
2022-01-15 01:42:36 +0700

    Create Project

---
+++ AndroidStudioProjects/Khoa/RNative04/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const App = () => {
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text style={{fontSize: 24, fontWeight: '700'}}>
+        Hello React Native Application
+      </Text>
+    </View>
+  );
+};
+
+export default App;
