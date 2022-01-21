5245187fc019b486f2a54c09c06c17f1f884b65c
KTheAsianimeBoi <86539636+KTheAsianimeBoi@users.noreply.github.com>
2022-01-22 03:54:46 +0700

    BuoiNam

---
+++ AndroidStudioProjects/Khoa/RNative04/src/HolderComponent/BaiTapBuoi5.js
@@ -0,0 +1,12 @@
+import {Text, View} from 'react-native';
+import React, {Component} from 'react';
+
+export default class BaiTapBuoi5 extends Component {
+  render() {
+    return (
+      <View>
+        <Text></Text>
+      </View>
+    );
+  }
+}
