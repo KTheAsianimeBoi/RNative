71dad7cef0f4eb1e48f5906e750b0f4eee860511
KTheAsianimeBoi <86539636+KTheAsianimeBoi@users.noreply.github.com>
2022-01-21 18:41:29 +0700

    BaiTapBuoiBon

--- AndroidStudioProjects/Khoa/RNative04/src/Components/BaiTapBon.js
+++ AndroidStudioProjects/Khoa/RNative04/src/Components/BaiTapBon.js
@@ -1,76 +1,69 @@
-import React, { Component } from 'react';
-import {View, Text,StyleSheet} from 'react-native';
+import React, {Component} from 'react';
+import {View, Text, StyleSheet} from 'react-native';
 
 export default class BaiTapBon extends Component {
-    render() {
-        return(
-            <View style= {styles.container}>
-                <View style={styles.green}>
-                    <View style={styles.yellow}>
-                     
-                    </View>
-                    <View style={styles.yellow}>
-                  
-                    </View>
-                </View>
-
-                <View style={styles.purple}>
-                    <View style={styles.blue}>
-                      
-                    </View>
-                    <View style={styles.blue}>
-                        
-                    </View>
-                    <View style={styles.blue}>
-                        
-                    </View>
-                    <View style={styles.blue}>
-                       
-                    </View>
-                </View>
+   render() {
+      return (
+         <View style={styles.container}>
+            <View style={styles.green}>
+               <View style={styles.yellow}></View>
+               <View style={styles.yellow}></View>
             </View>
-        ) 
-    }
+
+            <View stsyle={styles.purple}>
+               <View style={styles.blue}></View>
+               <View style={styles.blue}></View>s<View style={styles.blue}></View>
+               <View style={styles.blue}></View>
+               <View>
+                  <Text style={styles.textInfo}>
+                     I am gonna be sad if this does not work on the files! If I Try to do this,
+                     please
+                  </Text>
+               </View>
+            </View>
+         </View>
+      );
+   }
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex : 1,
-        justifyContent: 'center',
-    },
-    green: {
-        backgroundColor: '#A1c99A',
-        flex : 1,
-        borderBottomEndRadius:20,
-        borderBottomStartRadius:20,
-        marginBottom:20,
-        justifyContent:'space-around',
-    },
-    purple: {
-        backgroundColor: '#c8c8fA',
-        flex : 1,
-        borderTopStartRadius:20,
-        borderTopEndRadius:20,
-        flexDirection: 'row',
-        flexWrap: 'wrap',
-        alignContent: 'space-around',
-        justifyContent: 'space-around',
-    },
-    yellow:{
-        flex : 1,
-        backgroundColor: '#ffffc2',
-        borderRadius:20,
-        margin: 30,
-    },
-    blue:{
-        backgroundColor: '#4f82c0',
-        borderRadius:15,
-        width: '35%',
-        height: '35%',
-        margin :10,
-    },
-    textInfo:{
-        fontSize: 25,
-        fontWeight: 'bold',
-    }
-})
+   container: {
+      flex: 1,
+      justifyContent: 'center',
+   },
+   green: {
+      backgroundColor: '#A1c99A',
+      flex: 1,
+      borderBottomEndRadius: 20,
+      borderBottomStartRadius: 20,
+      marginBottom: 20,
+      justifyContent: 'space-around',
+   },
+   purple: {
+      backgroundColor: '#c8c8fA',
+      flex: 1,
+      borderTopStartRadius: 20,
+      borderTopEndRadius: 20,
+      flexDirection: 'row',
+      flexWrap: 'wrap',
+      alignContent: 'space-around',
+      justifyContent: 'space-around',
+   },
+   yellow: {
+      flex: 1,
+      backgroundColor: '#ffffc2',
+      borderRadius: 20,
+      margin: 30,
+   },
+   blue: {
+      backgroundColor: '#4f82c0',
+      borderRadius: 15,
+      width: '35%',
+      height: '35%',
+      margin: 10,
+   },
+   textInfo: {
+      fontSize: 25,
+      fontWeight: 'bold',
+   },
+});
